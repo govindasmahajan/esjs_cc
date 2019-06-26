cf78743a2adc80d63b9fb190bc843e51cba5614f
Govinda Mahajan <govindam@pune.smartek21.st21>
2019-06-26 16:29:49 +0530

    app.get by id

---
+++ Members.js
@@ -0,0 +1,19 @@
+const members = [
+  {
+    id: 1,
+    name: "Govinda",
+    email: "govinda@gmail.com"
+  },
+  {
+    id: 2,
+    name: "Mahajan",
+    email: "mahajana@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Govinda Mahajan",
+    email: "gm@gmail.com"
+  }
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -3,9 +3,29 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const members = require("./Members");
+const logger = require("./middleware/logger");
+
+app.use(logger);
+
 app.get("/", (req, res) => {
   res.send("<h1> ExpressJS </h1>");
 });
+
+/**Get One Member by ID */
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No member with the id of ${req.params.id} is present.` });
+  }
+});
+
+app.get("/api/members", (req, res) => res.json(members));
 
 /** Set a static folder */
 app.use(express.static(path.join(__dirname, "public")));
